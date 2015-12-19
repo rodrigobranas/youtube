fd3c8f6125888261b6563404ec2cfae49c5e96e3
Paulo Roberto <psface1@gmail.com>
2015-12-19 12:56:33 -0200

    add api em php

---
+++ AngularJS_6_Integrando_com_o_Backend/backend/contatosBackend.php
@@ -0,0 +1,30 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+$data = Date('d/m/Y h:i');
+
+$operadoras[] = array('nome' => 'Oi', 'codigo' => 14, 'categoria' => 'Celular', 'preco' => 2);
+$operadoras[] = array('nome' => 'Vivo', 'codigo' => 15, 'categoria' => 'Celular', 'preco' => 1);
+$operadoras[] = array('nome' => 'Tim', 'codigo' => 41, 'categoria' => 'Celular', 'preco' => 3);
+
+$contatos[] = array('nome' => 'Paulo', 'telefone' => '9999-8888', 'data' => $data, 
+					'operadora' => $operadoras[0]);
+$contatos[] = array('nome' => 'Ana', 'telefone' => '7777-8888', 'data' => $data, 
+					'operadora' => $operadoras[1]);
+$contatos[] = array('nome' => 'Maria', 'telefone' => '4444-8888', 'data' => $data, 
+					'operadora' => $operadoras[2]);
+
+$post = file_get_contents("php://input");
+$contato = json_decode($post);
+if(!empty($post)){
+	$contato = get_object_vars($contato);
+	$contato['operadora'] = get_object_vars($contato['operadora']);
+	$array_contatos = unserialize($_COOKIE['api_contatos']);
+	$array_contatos[] = $contato;
+	setcookie('api_contatos', serialize($array_contatos));
+}
+
+if(isset($_COOKIE['api_contatos'])){
+	$contatos = array_merge($contatos, unserialize($_COOKIE['api_contatos']));
+}
+
+echo json_encode($contatos);
