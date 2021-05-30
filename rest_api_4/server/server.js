901cf097b91e95a533b331258df35bef635bdabe
Gustavo Alexandre Nobre Mesquita <65964697+Gustavonobreza@users.noreply.github.com>
2021-05-30 17:28:13 -0300

    Update server.js

--- rest_api_4/server/server.js
+++ rest_api_4/server/server.js
@@ -5,12 +5,12 @@ app.use(express.json());
 app.use('/', require('./route/postsRoute'));
 app.use(function (error, req, res, next) {
 	if (error.message === 'Post already exists') {
-		return res.status(409).send(e.message);
+		return res.status(409).send(error.message);
 	}
 	if (error.message === 'Post not found') {
-		return res.status(404).send(e.message);
+		return res.status(404).send(error.message);
 	}
-	res.status(500).send(e.message);
+	res.status(500).send(error.message);
 });
 
 app.listen(3000);
